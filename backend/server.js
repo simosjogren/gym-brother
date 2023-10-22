ff624813eea01c0fd5f10e15134f0bf9406d3f79
Simo <simo.sjogren@tuni.fi>
2023-10-22 22:48:50 +0200

    Middlewared all the login-functionalities out of the main server.js-file.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const exphsb = require('express-handlebars');
 const path = require('path');
-const corsMiddleware = require('./middleware'); // Adjust the path as needed
+
+const corsMiddleware = require('./middleware');
+const userRouter = require('./userRoutes');
 
 const db = require('./config/database');
 const credentials = require('./config/credentials_connection');
@@ -30,10 +32,11 @@ db.sync()
         console.error('Error syncing database:', error);
     });
 
+
 const app = express();
 app.use(bodyParser.json());
-// Use the CORS middleware
-app.use(corsMiddleware);
+app.use(corsMiddleware); // CORS middleware
+app.use(userRouter); // User login middleware
 
 app.post('/post-workout', (req, res) => {
     console.log('Received /post-workout command.')
@@ -50,84 +53,6 @@ app.get('/get-workout', (req, res) => {
     // TODO make a query to the database which retrieves the user's last workout.
 
     res.json({ testData })
-});
-
-app.post('/users', async (req,res) => {
-    try {
-        console.log('Post request to /users')
-        console.log("Creating new account with username " + req.body.username)
-        const hashedPassword = await bcrypt.hash(req.body.password, 10) // Salt with number 10
-        const user = {'username': req.body.username, 'password': hashedPassword}
-        console.log('Hashed password: ' + hashedPassword)
-    
-        // TODO: Check if account already exists or not in the database.
-    
-        // Inserts user's hashed credentials to the database.
-        credentials.create({
-            id: user.username,
-            password: user.password
-        }).then(user => {
-            console.log('User created:', user);
-        }).catch(error => {
-            console.error('Error creating user:', error);
-        });
-    
-        // TODO: Create a new table to the database according to username & password.
-    
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
-})
-
-app.post('/users/login', async (req,res) => {
-    const user = {
-        username: req.body.username,
-        password: req.body.password
-    }
-    console.log('Post request to /users/login')
-    
-    // SQL-query for given username
-    credentials.findOne({
-        where: { id: user.username }
-    })
-    .then(retrievedDBUser => {
-        console.log(retrievedDBUser.createdAt)
-        // Checking if the user is found.
-        if (retrievedDBUser) {
-            try {
-                // Password comparision
-                bcrypt.compare(user.password, retrievedDBUser.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        // Correct password
-                        console.log('Password was correct.')
-            
-                        // TODO send cookie
-        
-                        res.status(200).send()
-                    } else {
-                        // Wrong password
-                        console.log('Password was INCORRECT.')
-                        res.status(401).send()
-                    }
-                })
-                .catch(error => {
-                    console.error('Error comparing passwords:', error);
-                    res.status(500).send();
-                });
-            } catch (error) {
-                console.error('Error:', error);
-                res.status(500).send();
-            }
-        } else {
-            res.status(404).send('User not found');
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching user:', error);
-        res.status(500).send('Error fetching user');
-    });
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ backend/userRoutes.js
@@ -0,0 +1,88 @@
+// Middleware for the login-functionality
+// Simo Sjögren
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const credentials = require('./config/credentials_connection');
+
+const userRouter = express.Router();
+
+userRouter.post('/users', async (req,res) => {
+    try {
+        console.log('Post request to /users')
+        console.log("Creating new account with username " + req.body.username)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10) // Salt with number 10
+        const user = {'username': req.body.username, 'password': hashedPassword}
+        console.log('Hashed password: ' + hashedPassword)
+    
+        // TODO: Check if account already exists or not in the database
+    
+        // Inserts user's hashed credentials to the database
+        credentials.create({
+            id: user.username,
+            password: user.password
+        }).then(user => {
+            console.log('User created:', user);
+        }).catch(error => {
+            console.error('Error creating user:', error);
+        });
+    
+        // TODO: Create a new table to the database according to username & password
+    
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+});
+
+userRouter.post('/users/login', async (req,res) => {
+    const user = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    console.log('Post request to /users/login')
+    
+    // SQL-query for given username
+    credentials.findOne({
+        where: { id: user.username }
+    })
+    .then(retrievedDBUser => {
+        console.log(retrievedDBUser.createdAt)
+        // Checking if the user is found
+        if (retrievedDBUser) {
+            try {
+                // Password comparision
+                bcrypt.compare(user.password, retrievedDBUser.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // Correct password
+                        console.log('Password was correct.')
+            
+                        // TODO send cookie
+        
+                        res.status(200).send()
+                    } else {
+                        // Wrong password
+                        console.log('Password was INCORRECT.')
+                        res.status(401).send()
+                    }
+                })
+                .catch(error => {
+                    console.error('Error comparing passwords:', error);
+                    res.status(500).send();
+                });
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).send();
+            }
+        } else {
+            res.status(404).send('User not found');
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching user:', error);
+        res.status(500).send('Error fetching user');
+    });
+});
+
+module.exports = userRouter;
